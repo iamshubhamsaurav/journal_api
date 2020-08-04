888354a7e54f6090beccec15e7958ab1bf7da273
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-04 19:08:12 +0530

    Integrated Security Packages

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const colors = require('colors');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
 const connectDB = require('./config/db');
 const errorHandler = require('./utils/errorHandler');
 const AppError = require('./utils/appError');
-
 const journalRoute = require('./routes/journals');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/auth');
+
+//Security Modules
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 process.on('uncaughtException', (err) => {
   console.log('UNHANDLED REJECTION! Shutting down...');
@@ -29,6 +35,25 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// ****** Security Middlewares ********
+//Sanitize data
+app.use(mongoSanitize());
+//Set Security headers
+app.use(helmet());
+//Prevent XSS - Cross site scripting attack
+app.use(xssClean());
+// Limiting requests
+app.use(
+  rateLimiter({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100, //  Max 100 request in the set duration
+  })
+);
+// Prevent Http params pollution
+app.use(hpp());
+// Enable Cross Site Resource Sharing
+app.use(cors());
 
 app.use('/api/v1/journals', journalRoute);
 app.use('/api/v1/auth', authRoute);
